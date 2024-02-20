6ea2c1bdc49662953ebb314d1fb8ea245e39e3f0
KaganAsl <kagan.aslancan@gmail.com>
2024-02-20 21:13:45 +0300

    Closures and iterators added

--- src/lib.rs
+++ src/lib.rs
@@ -28,60 +28,40 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
 
-        let ignore_case;
-        let hard_case;
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
 
-        if env::var("IGNORE_CASE").is_ok() {
-            ignore_case = true;
-            hard_case = false;
-        } else if env::var("HARD_CASE").is_ok() {
-            ignore_case = false;
-            hard_case = true;
-        } else {
-            ignore_case = false;
-            hard_case = false;
-        }
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+        let hard_case = env::var("HARD_CASE").is_ok();
 
         Ok(Config {query, file_path, ignore_case, hard_case})
     }
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents.lines().filter(|line| line.contains(query)).collect()
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+
+    contents.lines().filter(|line| line.to_lowercase().contains(&query)).collect()
+}
+
+pub fn search_case_hard_sensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
 
     for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
-}
-
-pub fn search_case_insensitive<'a>(query: &str, contens: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in contens.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
-}
-
-pub fn search_case_hard_sensitive<'a>(query: &str, contens: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contens.lines() {
         for word in line.split_whitespace() {
             if word.eq(query) {
                 results.push(line);
